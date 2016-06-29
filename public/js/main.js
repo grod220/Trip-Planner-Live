ec5e9d209073c5aa5c79ec582518cc854205967e
Gabe Rodriguez <grod220@gmail.com>
2016-06-29 12:45:06 -0400

    moving right along

--- public/js/main.js
+++ public/js/main.js
@@ -46,18 +46,73 @@ $(function initializeMap (){
     activity: '/images/star-3.png'
   };
 
-  function drawMarker (type, coords) {
+  var holderArr = [];
+
+  function drawMarker (type, coords, text) {
     var latLng = new google.maps.LatLng(coords[0], coords[1]);
     var iconURL = iconURLs[type];
     var marker = new google.maps.Marker({
+      textHold: text,
       icon: iconURL,
-      position: latLng
+      position: latLng,
+      animation: google.maps.Animation.DROP
     });
+    holderArr.push(marker);
     marker.setMap(currentMap);
   }
+
 
   // drawMarker('hotel', [40.705137, -74.007624]);
   // drawMarker('restaurant', [40.705137, -74.013940]);
   // drawMarker('activity', [40.716291, -73.995315]);
 
+
+      for (var i=0; i<hotels.length; i++) {
+        $('#hotel-choices').append('<option>' + hotels[i].name + '</option>');
+      }
+      for (var i=0; i<restaurants.length; i++) {
+        $('#restaurant-choices').append('<option>' + restaurants[i].name + '</option>');
+      }
+      for (var i=0; i<activities.length; i++) {
+        $('#activity-choices').append('<option>' + activities[i].name + '</option>');
+      }
+
+      function selectFindLocation (database, typeicon, dropDownID,childNum) {
+        $(dropDownID).next().on('click', function(event){
+          var selectedText = $(dropDownID).find('option:selected').text();
+          $('#itinerary>div:nth-child('+childNum+')').append('<div data-id="2" class="itinerary-item"><span class="title">' + selectedText + '</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+          for (var i=0; i<database.length; i++) {
+              if (selectedText === database[i].name) {
+                drawMarker(typeicon, [database[i].place.location[0], database[i].place.location[1]], selectedText);
+                return;
+            }
+          }
+        });
+      }
+    selectFindLocation(hotels, 'hotel', '#hotel-choices',1);
+    selectFindLocation(restaurants, 'restaurant', '#restaurant-choices',2);
+    selectFindLocation(activities, 'activity', '#activity-choices',3);
+
+    function remove () {
+      $('#itinerary>div').on('click', '.remove', function(event){
+        var leftText = $(this).parent().text().slice(0,-2);
+        for (var i=0; i<holderArr.length; i++) {
+          if(holderArr[i].textHold === leftText) {
+            holderArr[i].setMap(null);
+          }
+        }
+        $(this).parent()[0].remove();
+      });
+    }
+    remove();
+
+    function addDay (){
+      $('#day-add').on('click', function (event){
+        $('#day-add').prepend('<button class="btn btn-circle day-btn current-day">1</button>')
+      });
+    }
+
+
 });
+
+
